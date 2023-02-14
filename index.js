095f8db4a3406a27f7e7d3f238b6294e5cea3336
ValeraBezriadin <va1era.bezryadin.ua@gmail.com>
2023-02-14 10:57:04 +0200

    11.02

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-const menuButton = document.querySelector(".product__active")
-const menuList = document.querySelector(".nav__product")
- 
-menuButton.addEventListener("click", ()=>{
-    menuList.classList.toggle("nav__product--active")
-})
+const menuButton = document.querySelector(".product__active");
+const menuList = document.querySelector(".nav__product");
+
+menuButton.addEventListener("click", () => {
+  menuList.classList.toggle("nav__product--active");
+});
+
+const swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
