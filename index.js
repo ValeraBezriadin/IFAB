76d21118b8ed2b8486ea21adb8e9686ac0c32b25
ValeraBezriadin <va1era.bezryadin.ua@gmail.com>
2023-02-19 20:14:25 +0200

    adaptuv section hero

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-const menuButton = document.querySelector(".product__active");
-const menuList = document.querySelector(".nav__product");
+const menuButton = document.querySelector(".nav__button-menu");
+const menuList = document.querySelector(".nav__list");
+const sabButton = document.querySelector(".product__active");
+const subList = document.querySelector(".nav__product");
 
-menuButton.addEventListener("click", () => {
-  menuList.classList.toggle("nav__product--active");
+
+menuButton.addEventListener("click", ()=> {
+  menuButton.classList.toggle("nav__button-active");
+  menuList.classList.toggle("nav__list-active");
+});
+
+sabButton.addEventListener("click", () => {
+  subList.classList.toggle("nav__product--active");
 });
 
 const swiper = new Swiper(".mySwiper", {
